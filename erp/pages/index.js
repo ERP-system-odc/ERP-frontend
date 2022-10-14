b472911795ca49f0f89a1cd11aa4d2ef9af2eeeb
fraolb <Fraolbereket@gmail.com>
2022-10-14 10:26:56 +0300

    login page

--- erp/pages/index.js
+++ erp/pages/index.js
@@ -6,7 +6,7 @@ import { loginUser } from "../lib/auth";
 
 
 const index = () => {
-  const [login, setLogin] = useState({ username: "", email: "" });
+  const [login, setLogin] = useState({ email: "", password: "" });
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -18,12 +18,33 @@ const index = () => {
     e.preventDefault();
     console.log(login);
 
-    const { username, email}= login;
+    const { email, password}= login;
 
-    //Pass(login)
-    loginUser(username, email).then(()=>{
-      Router.push('/profile')
+    fetch("http://localhost:5001/api/auth/signin", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+       
+        email: login.email,
+        
+        password: login.password
+      
+      })
     })
+    .then((response) => response.json())
+    .then(data => {
+        //handle data
+        console.log(data);
+        console.log(data.phonenumber);
+        Router.push('/profile')
+      })
+      .catch(error => {
+        //handle error
+      });
+    //Pass(login)
+    // loginUser(email, password).then(()=>{
+    //   Router.push('/profile')
+    // })
    
   };
   return (
@@ -35,24 +56,24 @@ const index = () => {
       <article>
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
-            <label htmlFor="firstName">username : </label>
+            <label htmlFor="email">Email : </label>
             <input
-              type="text"
-              id="username"
-              name="username"
-              value={login.username}
+              type="email"
+              id="email"
+              name="email"
+              value={login.email}
               required
               minLength="4"
               onChange={handleChange}
             />
           </div>
           <div className="form-control">
-            <label htmlFor="email">email : </label>
+            <label htmlFor="password">password : </label>
             <input
-              type="email"
-              id="email"
-              name="email"
-              value={login.email}
+              type="password"
+              id="password"
+              name="password"
+              value={login.password}
               onChange={handleChange}
             />
           </div>
