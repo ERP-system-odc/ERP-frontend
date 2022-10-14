d0aa76e95334067bf1de05af42a1a7524e9a6795
fraolb <Fraolbereket@gmail.com>
2022-10-14 09:32:53 +0300

    page that user enters when signed in

--- erp/pages/profile.js
+++ erp/pages/profile.js
@@ -1,16 +1,35 @@
-import React from "react";
-import { getUserProfile } from "/lib/auth";
-
-
-export default class Profile extends React.Component {
-    state ={
-        user: null
-    };
-    componentDidMount(){
-        getUserProfile().then(user =>this.setState({user}));
-    }
-    render(){
-    return <pre>{JSON.stringify(this.state.user, null, 2)}</pre>;
-    }
+const Profile = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
 }
+
+export default Profile;
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { getUserProfile } from "/lib/auth";
+
+
+// export default class Profile extends React.Component {
+//     state ={
+//         user: null
+//     };
+//     componentDidMount(){
+//         getUserProfile().then(user =>this.setState({user}));
+//     }
+//     render(){
+//     return <pre>{JSON.stringify(this.state.user, null, 2)}</pre>;
+//     }
+// }
+
 
