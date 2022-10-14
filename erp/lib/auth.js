c8705951d796ad644373fa633329aab55372d45f
fraolb <Fraolbereket@gmail.com>
2022-10-14 10:26:30 +0300

    axios

--- erp/lib/auth.js
+++ erp/lib/auth.js
@@ -2,13 +2,20 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
-export const loginUser = async (username, email)=>{
-    const { data }= await axios.post('https://jsonplaceholder.typicode.com/users',{username, email});
+export const loginUser = async (email, password)=>{
+    const { data }= await axios.post('http://localhost:5001/api/auth/signin',{email, password});
     console.log(data);
     console.log(data.email)
     
 }
 export const getUserProfile = async ()=>{
-    const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+    const {data} = await axios.get('http://localhost:5001/api/auth/signin');
     return data;
 }
+
+
+export const signinUser = async (signin)=>{
+    const { data }= await axios.post('http://localhost:5001/api/auth/signup',{signin});
+    console.log(data);
+    
+} 
