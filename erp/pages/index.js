a4d5b11c8d7f108382c3d0d91d0ae01b6887058c
fraolb <Fraolbereket@gmail.com>
2022-10-18 21:36:06 +0300

    sign in MUI

--- erp/pages/index.js
+++ erp/pages/index.js
@@ -1,101 +1,166 @@
-import Head from "next/head";
-import { useState, useEffect } from "react";
-import Link from "next/link";
-import Router from "next/router";
-import { loginUser } from "../lib/auth";
+import Head from 'next/head';
+import NextLink from 'next/link';
+import Router from 'next/router';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email('Must be a valid email')
+        .max(255)
+        .required('Email is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required('Password is required')
+    }),
+    onSubmit: values => {
+      fetch("http://localhost:5000/api/auth/signin", {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(values, null, 2)
+          })
+      .then((response) => response.json())
+      .then(data => {
+          //handle data
+          console.log(data);
+          console.log(data.credentials.phonenumber);
+          console.log(data.credentials.fullname)
+          const name=data.credentials.fullname;
+          const phone=data.credentials.phonenumber;
+          Router.push({
+            pathname: '/profile',
+            query: { name, phone}
+        })
+        })
+        .catch(error => {
+          //handle error
+        });
+    
+  },
 
-const index = () => {
-  const [login, setLogin] = useState({ email: "", password: "" });
+  });
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setLogin({ ...login, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(login);
-
-    const { email, password}= login;
-
-    fetch("http://localhost:5001/api/auth/signin", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({
-       
-        email: login.email,
-        
-        password: login.password
-      
-      })
-    })
-    .then((response) => response.json())
-    .then(data => {
-        //handle data
-        console.log(data);
-        console.log(data.phonenumber);
-        console.log(data.credentials.fullname)
-        const name=data.credentials.fullname;
-        const phone=data.credentials.phonenumber;
-        Router.push({
-          pathname: '/profile',
-          query: { name, phone}
-      })
-      })
-      .catch(error => {
-        //handle error
-      });
-
-      setLogin({ email: "", password: "" })
-    //Pass(login)
-    // loginUser(email, password).then(()=>{
-    //   Router.push('/profile')
-    // })
-   
-  };
   return (
     <>
       <Head>
-        <title>ERP System</title>
-        
+        <title>Login | ERP</title>
       </Head>
-      <article className="frontPage" style={{marginRight:"0"}}>
-        <div>
-          <img src="frontpage.png"/>
-        </div>
-        <form className="form" style={{marginLeft:"0"}} onSubmit={handleSubmit} >
-          <h2>ERP System</h2>
-          <div className="form-control">
-            <label htmlFor="email">Email : </label>
-            <input
-              type="email"
-              id="email"
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%'
+        }}
+      >
+        <Container maxWidth="sm">
+         
+          <form onSubmit={formik.handleSubmit}>
+            <Box sx={{ my: 3 }}>
+              <Typography
+                color="textPrimary"
+                variant="h4"
+              >
+                Sign in
+              </Typography>
+              <Typography
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
+                Sign in on the internal platform
+              </Typography>
+            </Box>
+            
+            <Box
+              sx={{
+                pb: 1,
+                pt: 3
+              }}
+            >
+              <Typography
+                align="center"
+                color="textSecondary"
+                variant="body1"
+              >
+                Login with email address
+              </Typography>
+            </Box>
+            <TextField
+              error={Boolean(formik.touched.email && formik.errors.email)}
+              fullWidth
+              helperText={formik.touched.email && formik.errors.email}
+              label="Email Address"
+              margin="normal"
               name="email"
-              value={login.email}
-              required
-              minLength="4"
-              onChange={handleChange}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              value={formik.values.email}
+              variant="outlined"
             />
-          </div>
-          <div className="form-control">
-            <label htmlFor="password">password : </label>
-            <input
-              type="password"
-              id="password"
+            <TextField
+              error={Boolean(formik.touched.password && formik.errors.password)}
+              fullWidth
+              helperText={formik.touched.password && formik.errors.password}
+              label="Password"
+              margin="normal"
               name="password"
-              required
-              value={login.password}
-              onChange={handleChange}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="password"
+              value={formik.values.password}
+              variant="outlined"
             />
-          </div>
-          <button type="Submit" className="btn">sign in</button>
-          <p>Dont have an account <Link href="/signup">Sign Up</Link></p>
-        </form>
-      </article>
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                disabled={formik.isSubmitting}
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+               Login
+              </Button>
+            </Box>
+            <Typography
+              color="textSecondary"
+              variant="body2"
+            >
+              Don&apos;t have an account?
+              {' '}
+              <NextLink
+                href="/register"
+              >
+                <Link
+                  to="/register"
+                  variant="subtitle2"
+                  underline="hover"
+                  sx={{
+                    cursor: 'pointer'
+                  }}
+                >
+                  Sign Up
+                </Link>
+              </NextLink>
+            </Typography>
+          </form>
+        </Container>
+      </Box>
     </>
   );
 };
 
-export default index;
+export default Login;
