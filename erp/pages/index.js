427ddec3a726a94baf3407370d7d4452c3ea3891
fraolb <Fraolbereket@gmail.com>
2022-10-19 15:03:19 +0300

    sign in to the firm or the dashoard

--- erp/pages/index.js
+++ erp/pages/index.js
@@ -1,53 +1,73 @@
-import Head from 'next/head';
-import NextLink from 'next/link';
-import Router from 'next/router';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Head from "next/head";
+import NextLink from "next/link";
+import Router from "next/router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const Login = () => {
   const formik = useFormik({
     initialValues: {
-      email: '',
-      password: ''
+      email: "",
+      password: "",
     },
     validationSchema: Yup.object({
-      email: Yup
-        .string()
-        .email('Must be a valid email')
+      email: Yup.string()
+        .email("Must be a valid email")
         .max(255)
-        .required('Email is required'),
-      password: Yup
-        .string()
-        .max(255)
-        .required('Password is required')
+        .required("Email is required"),
+      password: Yup.string().max(255).required("Password is required"),
     }),
-    onSubmit: values => {
+    onSubmit: (values) => {
       fetch("http://localhost:5000/api/auth/signin", {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(values, null, 2)
-          })
-      .then((response) => response.json())
-      .then(data => {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values, null, 2),
+      })
+        .then((response) => response.json())
+        .then((data) => {
           //handle data
           console.log(data);
-          console.log(data.credentials.phonenumber);
-          console.log(data.credentials.fullname)
-          const name=data.credentials.fullname;
-          const phone=data.credentials.phonenumber;
-          Router.push({
-            pathname: '/profile',
-            query: { name, phone}
+          console.log(data.credentials.phone_number);
+          console.log(data.credentials.full_name);
+          console.log(data.credentials);
+          console.log(data["access-token"]);
+          // const jwt = data.access-token;
+
+          // console.log("the token is " )
+          // WIndow.localStorage.setItem("token", data["access-token"])
+          sessionStorage.setItem("token", data["access-token"]);
+          console.log(sessionStorage.getItem("token"));
+          const id= data.credentials.id;
+          console.log(id)
+          const name = data.credentials.full_name;
+          const phone = data.credentials.phone_number;
+          if (data.credentials.is_starter == true) {
+            Router.push({
+              pathname: "/firm",
+              query: { id, name, phone },
+            });
+          } else {
+            Router.push({
+              pathname: "/profile",
+              query: { id,name, phone },
+            });
+          }
         })
-        })
-        .catch(error => {
+
+        .catch((error) => {
           //handle error
         });
-    
-  },
-
+    },
   });
 
   return (
@@ -58,42 +78,30 @@ const Login = () => {
       <Box
         component="main"
         sx={{
-          alignItems: 'center',
-          display: 'flex',
+          alignItems: "center",
+          display: "flex",
           flexGrow: 1,
-          minHeight: '100%'
+          minHeight: "100%",
         }}
       >
         <Container maxWidth="sm">
-         
           <form onSubmit={formik.handleSubmit}>
             <Box sx={{ my: 3 }}>
-              <Typography
-                color="textPrimary"
-                variant="h4"
-              >
+              <Typography color="textPrimary" variant="h4">
                 Sign in
               </Typography>
-              <Typography
-                color="textSecondary"
-                gutterBottom
-                variant="body2"
-              >
+              <Typography color="textSecondary" gutterBottom variant="body2">
                 Sign in on the internal platform
               </Typography>
             </Box>
-            
+
             <Box
               sx={{
                 pb: 1,
-                pt: 3
+                pt: 3,
               }}
             >
-              <Typography
-                align="center"
-                color="textSecondary"
-                variant="body1"
-              >
+              <Typography align="center" color="textSecondary" variant="body1">
                 Login with email address
               </Typography>
             </Box>
@@ -132,24 +140,18 @@ const Login = () => {
                 type="submit"
                 variant="contained"
               >
-               Login
+                Login
               </Button>
             </Box>
-            <Typography
-              color="textSecondary"
-              variant="body2"
-            >
-              Don&apos;t have an account?
-              {' '}
-              <NextLink
-                href="/register"
-              >
+            <Typography color="textSecondary" variant="body2">
+              Don&apos;t have an account?{" "}
+              <NextLink href="/register">
                 <Link
                   to="/register"
                   variant="subtitle2"
                   underline="hover"
                   sx={{
-                    cursor: 'pointer'
+                    cursor: "pointer",
                   }}
                 >
                   Sign Up
