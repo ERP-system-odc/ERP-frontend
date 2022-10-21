fe75aefd0a73a8b6f04cd3b61dd7e7c3c2d7be1e
fraolb <Fraolbereket@gmail.com>
2022-10-21 19:58:41 +0300

    side bar items

---
+++ erp/components/dashboard-sidebar.js
@@ -0,0 +1,225 @@
+import { useEffect } from 'react';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import { Box, Button, Divider, Drawer, Typography, useMediaQuery } from '@mui/material';
+import GroupIcon from '@mui/icons-material/Group';
+import LogoutIcon from '@mui/icons-material/Logout';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import { ChartBar as ChartBarIcon } from '../icons/chart-bar';
+import CategoryIcon from '@mui/icons-material/Category';
+import { Cog as CogIcon } from '../icons/cog';
+import { Lock as LockIcon } from '../icons/lock';
+import { Selector as SelectorIcon } from '../icons/selector';
+import { ShoppingBag as ShoppingBagIcon } from '../icons/shopping-bag';
+import { User as UserIcon } from '../icons/user';
+import { UserAdd as UserAddIcon } from '../icons/user-add';
+import { Users as UsersIcon } from '../icons/users';
+import { XCircle as XCircleIcon } from '../icons/x-circle';
+import { Logo } from './logo';
+import { NavItem } from './nav-item';
+
+const items = [
+  {
+    href: '/profile',
+    icon: (<ChartBarIcon fontSize="small" />),
+    title: 'Dashboard'
+  },
+  {
+    href: '/customers',
+    icon: (<CategoryIcon fontSize="small" />),
+    title: 'Items'
+  },
+  {
+    href: '/products',
+    icon: (<ShoppingBagIcon fontSize="small" />),
+    title: 'Stocks'
+  },
+  {
+    href: '/account',
+    icon: (<GroupIcon fontSize="small" />),
+    title: 'Employees'
+  },
+  {
+    href: '/settings',
+    icon: (<CogIcon fontSize="small" />),
+    title: 'Settings'
+  },
+  {
+    href: '/',
+    icon: (<LogoutIcon fontSize="small" />),
+    title: 'Logout'
+  }
+];
+
+export const DashboardSidebar = (props) => {
+  const { open, onClose } = props;
+  const router = useRouter();
+  const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'), {
+    defaultMatches: true,
+    noSsr: false
+  });
+
+  useEffect(
+    () => {
+      if (!router.isReady) {
+        return;
+      }
+
+      if (open) {
+        onClose?.();
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [router.asPath]
+  );
+
+  const content = (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%'
+        }}
+      >
+        <div>
+          <Box sx={{ p: 3 }}>
+            <NextLink
+              href="/settings"
+              passHref
+            >
+              <a>
+                <Logo
+                  sx={{
+                    height: 42,
+                    width: 42
+                  }}
+                />
+              </a>
+            </NextLink>
+          </Box>
+          <Box sx={{ px: 2 }}>
+            <Box
+              sx={{
+                alignItems: 'center',
+                backgroundColor: 'rgba(255, 255, 255, 0.04)',
+                cursor: 'pointer',
+                display: 'flex',
+                justifyContent: 'space-between',
+                px: 3,
+                py: '11px',
+                borderRadius: 1
+              }}
+            >
+              <div>
+                <Typography
+                  color="inherit"
+                  variant="subtitle1"
+                >
+                  User Name
+                </Typography>
+                <Typography
+                  color="neutral.400"
+                  variant="body2"
+                >
+                  Your Firm
+                  {' '}
+                  : Name
+                </Typography>
+              </div>
+              <SelectorIcon
+                sx={{
+                  color: 'neutral.500',
+                  width: 14,
+                  height: 14
+                }}
+              />
+            </Box>
+          </Box>
+        </div>
+        <Divider
+          sx={{
+            borderColor: '#2D3748',
+            my: 3
+          }}
+        />
+        <Box sx={{ flexGrow: 1 }}>
+          {items.map((item) => (
+            <NavItem
+              key={item.title}
+              icon={item.icon}
+              href={item.href}
+              title={item.title}
+            />
+          ))}
+        </Box>
+        <Divider sx={{ borderColor: '#2D3748' }} />
+        <Box
+          sx={{
+            px: 2,
+            py: 3
+          }}
+        >
+          <Typography
+            color="neutral.100"
+            variant="subtitle2"
+          >
+            Need more features?
+          </Typography>
+          <Typography
+            color="neutral.500"
+            variant="body2"
+          >
+            All right reserved.
+          </Typography>
+          
+        </Box>
+      </Box>
+    </>
+  );
+
+  if (lgUp) {
+    return (
+      <Drawer
+        anchor="left"
+        open
+        PaperProps={{
+          sx: {
+            // backgroundColor: '#2196F3',
+            backgroundColor: 'neutral.900',
+            color: '#FFFFFF',
+            width: 280
+          }
+        }}
+        variant="permanent"
+      >
+        {content}
+      </Drawer>
+    );
+  }
+
+  return (
+    <Drawer
+      anchor="left"
+      onClose={onClose}
+      open={open}
+      PaperProps={{
+        sx: {
+          backgroundColor: 'neutral.900',
+          color: '#FFFFFF',
+          width: 280
+        }
+      }}
+      sx={{ zIndex: (theme) => theme.zIndex.appBar + 100 }}
+      variant="temporary"
+    >
+      {content}
+    </Drawer>
+  );
+};
+
+DashboardSidebar.propTypes = {
+  onClose: PropTypes.func,
+  open: PropTypes.bool
+};
