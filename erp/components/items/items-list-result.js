7b42fabaaab9cf75f20b254143e72d5308beefe2
fraolb <Fraolbereket@gmail.com>
2022-10-22 10:54:09 +0300

    item list fetcher

---
+++ erp/components/items/items-list-result.js
@@ -0,0 +1,198 @@
+import { useState } from 'react';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+import {
+  Avatar,
+  Box,
+  Card,
+  Checkbox,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography
+} from '@mui/material';
+import { getInitials } from '../../utils/get-initials';
+
+export const ItemsListResults = ({ customers, ...rest }) => {
+  const [selectedCustomerIds, setSelectedCustomerIds] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+
+  const handleSelectAll = (event) => {
+    let newSelectedCustomerIds;
+
+    if (event.target.checked) {
+      newSelectedCustomerIds = customers.map((customer) => customer.id);
+    } else {
+      newSelectedCustomerIds = [];
+    }
+
+    setSelectedCustomerIds(newSelectedCustomerIds);
+  };
+
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  async function getResponse() {
+    const response = await fetch(
+      "http://localhost:5000/api/inventory/manage",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "authorization": "Bearer "+sessionStorage.getItem("token"),
+          },
+          body: JSON.stringify(values, null, 2),
+        }
+    );
+    const dataItem = await response.json(); // Extracting data as a JSON Object from the response
+  }
+
+  const data = [
+    {
+      inv_id:1,
+      inventory_name: 'shoe',
+    inventory_price: '2500',
+    least_critical_amount: '2',
+    inventory_quantity: '10',
+    inventory_expense: '100'
+    },
+    {
+      inv_id:2,
+      inventory_name: 'Tshirt',
+    inventory_price: '500',
+    least_critical_amount: '4',
+    inventory_quantity: '20',
+    inventory_expense: '70'
+    },
+    {
+      inv_id:3,
+      inventory_name: 'Humain Hair',
+    inventory_price: '5000',
+    least_critical_amount: '2',
+    inventory_quantity: '6',
+    inventory_expense: '200'
+    },
+    {
+      inv_id:4,
+      inventory_name: 'Lip stick',
+    inventory_price: '50',
+    least_critical_amount: '10',
+    inventory_quantity: '100',
+    inventory_expense: '50'
+    },
+    {
+      inv_id:5,
+      inventory_name: 'Make Up',
+    inventory_price: '1000',
+    least_critical_amount: '3',
+    inventory_quantity: '8',
+    inventory_expense: '80'
+    },
+    {
+      inv_id:6,
+      inventory_name: 'Hoodie',
+    inventory_price: '800',
+    least_critical_amount: '4',
+    inventory_quantity: '15',
+    inventory_expense: '120'
+    }
+  ]
+
+  return (
+    <Card {...rest}>
+      <PerfectScrollbar>
+        <Box sx={{ minWidth: 1050 }}>
+        
+          <Table>
+            <TableHead>
+              <TableRow>
+               
+                <TableCell>
+                  Inventory Name
+                </TableCell>
+                <TableCell>
+                inventory_price
+                </TableCell>
+                <TableCell>
+                Minimum No. to get Notified
+                </TableCell>
+                <TableCell>
+                inventory_quantity
+                </TableCell>
+                <TableCell>
+                inventory_expense
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              
+              {
+              data.map((val) => (
+                <TableRow
+                  hover
+                  key={val.inv_id}
+                  selected={selectedCustomerIds.indexOf(val.inv_id) !== -1}
+                >
+                 
+                  <TableCell>
+                    <Box
+                      sx={{
+                        alignItems: 'center',
+                        display: 'flex'
+                      }}
+                    >
+                      
+                      <Typography
+                        color="textPrimary"
+                        variant="body1"
+                      >
+                        {val.inventory_name}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell>
+                    {val.inventory_price}
+                  </TableCell>
+                 
+                  <TableCell>
+                    {val.least_critical_amount}
+                  </TableCell>
+                  <TableCell>
+                    {val.inventory_quantity}
+                  </TableCell>
+                  <TableCell>
+                    {val.inventory_expense}
+                  </TableCell>
+                 
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </PerfectScrollbar>
+      <TablePagination
+        component="div"
+        count={customers.length}
+        onPageChange={handlePageChange}
+        onRowsPerPageChange={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
+};
+
+CustomerListResults.propTypes = {
+  customers: PropTypes.array.isRequired
+};
