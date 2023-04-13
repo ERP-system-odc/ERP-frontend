a5c48d88e74c73a03ba6fbbd8faefcf7dee33066
Fraol Bereket <81419937+fraolb@users.noreply.github.com>
2023-04-13 09:06:04 +0300

    rmv unnecessary

--- erp/pages/_document.js
+++ erp/pages/_document.js
@@ -25,29 +25,8 @@ class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto+Slab|Roboto:300,400,500,700&display=optional"
           />
           <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
             rel="icon"
             href="/favicon.ico"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <meta
-            name="theme-color"
-            content="#111827"
           />
         </Head>
         <body>
